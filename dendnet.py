5590b34ea15cb16c114465623260ae60f17f8774
Simon Forman <forman.simon@gmail.com>
2012-11-17 19:28:28 -0800

    Start on a register page.

--- dendnet.py
+++ dendnet.py
@@ -9,6 +9,11 @@ def hello_world():
     return 'Hello World!'
 
 
+@app.route("/register")
+def reg():
+    return render_template('register.html')
+
+
 if __name__ == '__main__':
     app.debug = True
     app.run(host='0.0.0.0')
