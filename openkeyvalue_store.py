d10a10feef4424ae782fdff4d7e5c6bd47ed3b48
Simon Forman <forman.simon@gmail.com>
2012-11-17 23:07:22 -0800

    simple caching for openkeyvalue.org

--- openkeyvalue_store.py
+++ openkeyvalue_store.py
@@ -4,12 +4,18 @@ import requests
 OKV = 'http://api.openkeyval.org/'
 
 
+CACHE = {}
+
+
 def store(**key_url):
-  t = requests.post(OKV, data=key_url)
-  return t.status_code == 200
+  CACHE.update(key_url)
+  return requests.post(OKV, data=key_url).status_code == 200
 
 
 def retrieve(key):
+  value = CACHE.get(key)
+  if value is not None:
+    return value
   r = requests.get(OKV + key)
   if r.status_code == 200:
     return r.text
