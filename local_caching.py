3556b79f12e9aafc99d1e26b1ced28aaf1268fcc
Simon Forman <forman.simon@gmail.com>
2012-11-24 23:26:50 -0800

    Clean up
    I was in a hurry and the imports were wrong, WRONG!

--- local_caching.py
+++ local_caching.py
@@ -1,7 +1,5 @@
 from os.path import exists
-from hashlib import md5
-from dendnet.memcache import Client
-from tagly import tag_for as gen_tag
+from memcache import Client
 from sekrit import MEMCACHE_SOCKET
 
 
@@ -9,10 +7,11 @@ if exists(MEMCACHE_SOCKET):
   CACHE = Client(['unix:' + MEMCACHE_SOCKET], debug=True)
 else:
   CACHE = Client(['127.0.0.1:11213'], debug=True)
+S = CACHE.set
+G = CACHE.get
 
 
 def store_dec(s):
-  S = CACHE.set
   def inner(tag, url):
     s(tag, url)
     S(tag, url)
@@ -20,8 +19,6 @@ def store_dec(s):
 
 
 def retrieve_dec(r):
-  S = CACHE.set
-  G = CACHE.get
   def inner(tag):
     url = G(tag)
     if url is None:
@@ -29,3 +26,4 @@ def retrieve_dec(r):
       S(tag, url)
     return url
   return inner
+
