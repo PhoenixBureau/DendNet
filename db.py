d3540125f46e2dfadf215ad6dfb5e850f6f70939
Simon Forman <forman.simon@gmail.com>
2013-01-17 01:04:50 -0800

    Some retry code for the db.

--- db.py
+++ db.py
@@ -1,44 +1,75 @@
+import logging
 from datetime import datetime
 import psycopg2
 from sekrit import SOOPER_SEKRIT_PASSWORD
 
 
-db = psycopg2.connect(host='127.0.0.1',
-                      database='calroc_dendrite',
-                      user='calroc_dendrite',
-                      password=SOOPER_SEKRIT_PASSWORD,)
-db.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
+def get_conn():
+  db = psycopg2.connect(
+    host='127.0.0.1',
+    database='calroc_dendrite',
+    user='calroc_dendrite',
+    password=SOOPER_SEKRIT_PASSWORD,
+    )
+  db.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
+  return db
+
+
+db = get_conn()
+log = logging.getLogger(__name__)
+
+
+def get_cursor():
+  global db
+  try:
+    return db.cursor()
+  except:
+    log.exception("Had a problem getting a cursor, retrying...")
+    db = get_conn()
+    return db.cursor()
+
+
+def attempt(query, args):
+  cursor = get_cursor()
+  try:
+    cursor.execute(query, args)
+  except psycopg2.IntegrityError:
+    # Pass this up as we should only encounter it (harmlessly) when
+    # repeating a store() call.
+    raise
+  except:
+    log.exception("execution of query failed, retrying: %r %r", query, args)
+    cursor.execute(query, args)
+  return cursor
 
 
 def retrieve(tag):
-  cursor = db.cursor()
-  cursor.execute("""SELECT url FROM tags.tags WHERE tag = %s;""", (tag,))
+  cursor = attempt('SELECT url FROM tags.tags WHERE tag = %s;', (tag,))
   result = cursor.fetchone()
   return result[0] if result else ''
 
 
 def store(tag, url):
-  cursor = db.cursor()
   try:
-    cursor.execute(
-      """INSERT INTO tags.tags (tag, url) VALUES (%s, %s);""",
+    attempt(
+      'INSERT INTO tags.tags (tag, url) VALUES (%s, %s);',
       (tag, url)
       )
   except psycopg2.IntegrityError:
+    # Ignore duplicates.
     pass
 
 
 def bump(from_, what, to):
-  cursor = db.cursor()
-  cursor.execute(
-    """INSERT INTO tags.bumps ("when", "from", "what", "to") VALUES (%s, %s, %s, %s);""",
+  attempt(
+    'INSERT INTO tags.bumps ("when", "from", "what", "to")'
+    ' VALUES (%s, %s, %s, %s);',
     (datetime.utcnow(), from_, what, to)
     )
 
 
 def extract_graph(tag):
-  cursor = db.cursor()
-  cursor.execute(
+  cursor = attempt(
     """SELECT "when", "from", "to" FROM tags.bumps WHERE "what" = %s;""",
     (tag,)
     )
@@ -47,5 +78,3 @@ def extract_graph(tag):
     nodes.update((from_, to))
     links.append((when, from_, to))
   return list(nodes), links
-
-
